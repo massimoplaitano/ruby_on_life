260f40cd62200cfd978c260c543aa6d4dda88add
Massimo Plaitano <plaitano@gmail.com>
2022-01-20 00:25:13 +0100

    Styled sign in e sign up pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def logo_svg
+    '<svg class="h-8 w-auto sm:h-10 fill-cyan-600" viewBox="0 0 3 3" aria-hidden="true"><path d="m0 3v-1h2v-1h-1v-1h1v1h1v2z"/></svg>'.html_safe
+  end
 end
