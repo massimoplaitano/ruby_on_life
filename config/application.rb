36af79e5f59a906e74602411890b22ebaa07b650
Massimo Plaitano <plaitano@gmail.com>
2022-01-30 01:29:15 +0100

    Fix registration

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module RubyOnLife
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = 'Rome'
   end
 end
